9d5c72fc797148d794747de3ab3ba2d0ebbc67b3
Robert Dyer <psybers@gmail.com>
2012-11-16 11:00:30 -0600

    remove warning

--- compiler/src/java/sizzle/runtime/SizzleRunner.java
+++ compiler/src/java/sizzle/runtime/SizzleRunner.java
@@ -14,7 +14,6 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.compress.GzipCodec;
