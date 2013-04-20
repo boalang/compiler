a47c0ccdb841544196a87f6867fabe23a30da48b
Robert Dyer <rdyer@iastate.edu>
2013-04-20 15:34:53 -0500

    remove warning

--- compiler/src/java/boa/functions/BoaAstIntrinsics.java
+++ compiler/src/java/boa/functions/BoaAstIntrinsics.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.MapFile;
