dc23b9ceaff519fac49fb9f13b10b5976b06b7fb
Robert Dyer <rdyer@iastate.edu>
2014-04-02 15:55:09 -0500

    fix build errors

--- src/java/boa/compiler/BoaCompiler.java
+++ src/java/boa/compiler/BoaCompiler.java
@@ -2,10 +2,12 @@ package boa.compiler;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.InputStream;
 import java.io.IOException;
 import java.net.URL;
