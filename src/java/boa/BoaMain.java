48ddc2ed78ec5d0da64305c3855ea87547e12c9b
Hridesh Rajan <hridesh@cs.iastate.edu>
2015-12-04 19:34:05 -0600

    Make toplevel.

--- src/java/boa/BoaMain.java
+++ src/java/boa/BoaMain.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package boa.compiler;
+package boa;
 
 import java.io.IOException;
 import org.apache.commons.cli.CommandLine;
