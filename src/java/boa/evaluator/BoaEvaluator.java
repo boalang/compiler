3e194c9ba0f1e4c0abc6a7d7fa107cbb67ce6343
Hridesh Rajan <hridesh@cs.iastate.edu>
2015-12-09 15:54:16 -0600

    package fix.

--- src/java/boa/evaluator/BoaEvaluator.java
+++ src/java/boa/evaluator/BoaEvaluator.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package boa;
+package boa.evaluator;
 
 import java.io.IOException;
 import org.apache.commons.cli.CommandLine;
