6f4311ecb6ab8e8d0795c1702449db90f23a8ad1
nguyen_hoan <hoan@iastate.edu>
2016-01-29 23:39:36 -0600

    minor

--- src/java/boa/datagen/forges/github/Config.java
+++ src/java/boa/datagen/forges/github/Config.java
@@ -2,9 +2,9 @@ package boa.datagen.forges.github;
 
 public final class Config {
 
-	public static String[][] githubUsernames = {{"nguyenhoan"}};
+	public static String[][] githubUsernames = {{"isu1000000"}};
 	public static String githubUsername = "isu1000000";
-	public static String githubPassword = "github4moi";
+	public static String githubPassword = "isu123456";
 	
 	public static String githubRepoListDir = "github/repos-list";
 	public static String githubRepoMetadataDir = "github/repos";
