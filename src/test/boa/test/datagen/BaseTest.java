50841a4e7e3ef06a8fde416baed22cc08ad06b42
Robert Dyer <rdyer@bgsu.edu>
2016-02-01 09:43:24 -0500

    add initial test infrastructure for datagen

---
+++ src/test/boa/test/datagen/BaseTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016, Hridesh Rajan, Robert Dyer, 
+ *                 Iowa State University of Science and Technology,
+ *                 and Bowling Green State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package boa.test.datagen;
+
+import java.io.*;
+import java.util.*;
+
+import com.googlecode.protobuf.format.JsonFormat;
+
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.*;
+
+import boa.types.Ast.ASTRoot;
+
+import boa.datagen.util.JavaVisitor;
+
+public class BaseTest {
+	public static String parse(final String content) {
+		final ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(content.toCharArray());
+
+		final Map options = JavaCore.getOptions();
+		JavaCore.setComplianceOptions(JavaCore.VERSION_1_8, options);
+		parser.setCompilerOptions(options);
+
+		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+
+		final ASTRoot.Builder ast = ASTRoot.newBuilder();
+		final JavaVisitor visitor = new JavaVisitor(content, new HashMap<String, Integer>());
+		try {
+			ast.addNamespaces(visitor.getNamespaces(cu));
+			for (final String s : visitor.getImports())
+				ast.addImports(s);
+		} catch (final Exception e) {
+			System.err.println(e);
+			e.printStackTrace();
+			return "";
+		}
+
+		return JsonFormat.printToString(ast.build());
+	}
+}
---
+++ src/test/boa/test/datagen/TestLambda.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016, Hridesh Rajan, Robert Dyer, 
+ *                 Iowa State University of Science and Technology,
+ *                 and Bowling Green State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package boa.test.datagen;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestLambda extends BaseTest {
+	@Test
+	public void lambda() {
+		assertEquals(parse(
+			"public class LambdaExample {\n" +
+			"   public static void main(String args[]){\n" +
+			"      LambdaExample le= new LambdaExample();\n" +
+
+			"      //with type declaration\n" +
+			"      MathOperation addition = (int a, int b) -> a + b;\n" +
+			"      //with out type declaration\n" +
+			"      MathOperation subtraction = (a, b) -> a - b;\n" +
+
+			"      //with return statement along with curly braces\n" +
+			"      MathOperation multiplication = (int a, int b) -> { return a * b; };\n" +
+
+			"      //without return statement and without curly braces\n" +
+			"      MathOperation division = (int a, int b) -> a / b;\n" +
+
+			"      System.out.println(\"10 + 5 = \" + le.operate(10, 5, addition));\n" +
+			"      System.out.println(\"10 - 5 = \" + le.operate(10, 5, subtraction));\n" +
+			"      System.out.println(\"10 x 5 = \" + le.operate(10, 5, multiplication));\n" +
+			"      System.out.println(\"10 / 5 = \" + le.operate(10, 5, division));\n" +
+
+			"      //without parenthesis\n" +
+			"      GreetingService greetService1 = message ->\n" +
+			"      System.out.println(\"Hello \" + message);\n" +
+
+			"      //with parenthesis\n" +
+			"      GreetingService greetService2 = (message) ->\n" +
+			"      System.out.println(\"Hello \" + message);\n" +
+
+			"      //without arguments\n" +
+			"      System.out.println(\"Hello \" + () -> \"World\");\n" +
+
+			"      greetService1.sayMessage(\"Mahesh\");\n" +
+			"      greetService2.sayMessage(\"Suresh\");\n" +
+			"   }\n" +
+
+			"   interface MathOperation {\n" +
+			"      int operation(int a, int b);\n" +
+			"   }\n" +
+
+			"   interface GreetingService {\n" +
+			"      void sayMessage(String message);\n" +
+			"   }\n" +
+
+			"   private int operate(int a, int b, MathOperation mathOperation){\n" +
+			"      return mathOperation.operation(a, b);\n" +
+			"   }\n" +
+			"}"), null);
+	}
+}
