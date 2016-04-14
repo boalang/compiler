d5b531d759472e77705ebc74c5bcedb083513f31
farheen <farhees@bgsu.edu>
2016-04-13 20:58:12 -0400

    test case fix ConstructorMethodReference

--- src/test/boa/test/datagen/TestConstructorMethodReference.java
+++ src/test/boa/test/datagen/TestConstructorMethodReference.java
@@ -28,7 +28,85 @@ public class TestConstructorMethodReference extends BaseTest {
 	@Test
 	public void methodReference() {
 		assertEquals(parseWrapped(
-				"    ConstructorReference cref = Item::new;\n" +
-                "    Item item = cref.constructor();"), null);
+			"    ConstructorReference cref = Item::new;\n" +
+            "    Item item = cref.constructor();"), 
+			"{\n" +
+			"   \"namespaces\": [\n" +
+			"      {\n" +
+			"         \"name\": \"\",\n" +
+			"         \"declarations\": [\n" +
+			"            {\n" +
+			"               \"name\": \"t\",\n" +
+			"               \"kind\": \"CLASS\",\n" +
+			"               \"methods\": [\n" +
+			"                  {\n" +
+			"                     \"name\": \"m\",\n" +
+			"                     \"return_type\": {\n" +
+			"                        \"kind\": \"OTHER\",\n" +
+			"                        \"name\": 0\n" +
+			"                     },\n" +
+			"                     \"statements\": [\n" +
+			"                        {\n" +
+			"                           \"kind\": \"BLOCK\",\n" +
+			"                           \"statements\": [\n" +
+			"                              {\n" +
+			"                                 \"kind\": \"EXPRESSION\",\n" +
+			"                                 \"expression\": {\n" +
+			"                                    \"kind\": \"VARDECL\",\n" +
+			"                                    \"variable_decls\": [\n" +
+			"                                       {\n" +
+			"                                          \"name\": \"cref\",\n" +
+			"                                          \"variable_type\": {\n" +
+			"                                             \"kind\": \"OTHER\",\n" +
+			"                                             \"name\": 1\n" +
+			"                                          },\n" +
+			"                                          \"initializer\": {\n" +
+			"                                             \"kind\": \"METHOD_REFERENCE\",\n" +
+			"                                             \"new_type\": {\n" +
+			"                                                \"kind\": \"OTHER\",\n" +
+			"                                                \"name\": 2\n" +
+			"                                             },\n" +
+			"                                             \"method\": \"new\"\n" +
+			"                                          }\n" +
+			"                                       }\n" +
+			"                                    ]\n" +
+			"                                 }\n" +
+			"                              },\n" +
+			"                              {\n" +
+			"                                 \"kind\": \"EXPRESSION\",\n" +
+			"                                 \"expression\": {\n" +
+			"                                    \"kind\": \"VARDECL\",\n" +
+			"                                    \"variable_decls\": [\n" +
+			"                                       {\n" +
+			"                                          \"name\": \"item\",\n" +
+			"                                          \"variable_type\": {\n" +
+			"                                             \"kind\": \"OTHER\",\n" +
+			"                                             \"name\": 2\n" +
+			"                                          },\n" +
+			"                                          \"initializer\": {\n" +
+			"                                             \"kind\": \"METHODCALL\",\n" +
+			"                                             \"expressions\": [\n" +
+			"                                                {\n" +
+			"                                                   \"kind\": \"VARACCESS\",\n" +
+			"                                                   \"variable\": \"cref\"\n" +
+			"                                                }\n" +
+			"                                             ],\n" +
+			"                                             \"method\": \"constructor\"\n" +
+			"                                          }\n" +
+			"                                       }\n" +
+			"                                    ]\n" +
+			"                                 }\n" +
+			"                              }\n" +
+			"                           ]\n" +
+			"                        }\n" +
+			"                     ]\n" +
+			"                  }\n" +
+			"               ]\n" +
+			"            }\n" +
+			"         ]\n" +
+			"      }\n" +
+			"   ]\n" +
+			"}"
+		);
 	}
 }
