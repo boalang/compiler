4e6c158fd04a42f4f96c49e920dbf17e540a8a91
Hridesh Rajan <hridesh@users.sourceforge.net>
2017-02-07 13:44:51 -0600

    classpath addition for new components.

--- boa.sh
+++ boa.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(dirname "$0")
 
-java -cp .:$BASEDIR/dist/boa-compiler.jar:$BASEDIR/lib/hadoop-core-1.0.4.jar:$BASEDIR/lib/commons-lang-2.4.jar:$BASEDIR/lib/commons-math-2.1.jar boa.BoaMain $*
+java -cp ".:$BASEDIR/dist/boa-compiler.jar:$BASEDIR/lib/*:$BASEDIR/lib/evaluator/*:$BASEDIR/lib/datagen/*:$BASEDIR/compile" boa.BoaMain $*
