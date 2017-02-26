6ecf209fee0543ba09c5aae2df1eed99e0728441
nmtiwari <nmtiwari@iastate.edu>
2017-02-26 16:46:01 -0600

    Adding serialization support for the model using simple json

--- src/java/boa/aggregators/UserDefinedAggregator.java
+++ src/java/boa/aggregators/UserDefinedAggregator.java
@@ -2,39 +2,24 @@ package boa.aggregators;
 
 
 import boa.compiler.UserDefinedAggregators;
-import boa.datagen.DefaultProperties;
-import org.apache.commons.io.FileUtils;
-import org.eclipse.core.internal.utils.FileUtil;
+import boa.datagen.util.FileIO;
+import com.google.gson.Gson;
 
 import java.io.*;
-import java.nio.file.Files;
 
 @AggregatorSpec(name = "UserDefinedAgg", type = "UserDefined", canCombine = false)
 public abstract class UserDefinedAggregator extends Aggregator {
 
-	public void store(Object object, String className) {
+	public void store(Object object) {
+		Gson json = new Gson();
 		File output = new File(UserDefinedAggregators.getFileName());
 		final String dest= output.getAbsolutePath() + "/";
 		output.mkdir();
-		try {
-			FileOutputStream fos = new FileOutputStream(dest + UserDefinedAggregators.getFileName() + ".model");
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			oos.writeObject(object);
-			copyClassFile(className, dest);
-			oos.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		writeAsJSON(object, dest + UserDefinedAggregators.getFileName() + ".model");
 	}
 
-	private void copyClassFile(String className, String dest) {
-		className =  className.substring(4).replace(".", "$") + ".class"; //FIXME: given index 4 is an hack to avoid the "boa." from class name
-		File source = new File("./compile/boa/" + className);
-		File destination = new File(dest + "/" + className);
-		try{
-			FileUtils.copyFile(source, destination);
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
+	private void writeAsJSON(Object object, String path) {
+		Gson writer = new Gson();
+		FileIO.writeFileContents(new File(path), writer.toJson(object));
 	}
 }
