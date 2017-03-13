f7f033f2ebd3ae62a9afce38fb40fd78ce9aa925
Robert Dyer <rdyer@bgsu.edu>
2017-03-13 05:30:32 -0400

    add initial testing support for shadow types

---
+++ src/test/boa/test/compiler/TestShadowBad.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017, Hridesh Rajan, Robert Dyer, 
+ *                 Iowa State University of Science and Technology
+ *                 and Bowling Green State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package boa.test.compiler;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * @author rdyer
+ */
+@RunWith(JUnit4.class)
+public class TestShadowBad extends BaseTest {
+	final private static String rootDir = "test/shadow/";
+	final private static String badDir = rootDir + "errors/";
+
+
+	@Test
+	public void wrongField() throws IOException {
+		typecheck(load(badDir + "wrong-field.boa"), "'tuple [Expression, Statement, Statement]' has no member named 'statements'");
+	}
+}
