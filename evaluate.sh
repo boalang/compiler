c8bbd229158751dfa48b99eff130d130348300c9
Robert Dyer <rdyer@bgsu.edu>
2017-03-15 10:49:40 -0400

    make evaluator script more robust to inputs

--- evaluate.sh
+++ evaluate.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-if [ "$#" -eq 0 ]; then
+if [ "$#" -ne 2 ]; then
 	echo "Usage: $0 path/to/input.boa output-dir/ [options]"
+	exit -1
+fi
+
+if [ ! -f $1 ]; then
+	echo "input '$1' is not a file"
+	exit -2
+fi
+
+if [ -d $2 ]; then
+    read -n 1 -p "output directory '$2' exists - delete? [Y/n] " yn
+	echo ""
+
+	yn=`echo $yn | tr '[:upper:]' '[:lower:]'`
+
+	if [[ $yn =~ ^(y| ) ]] | [ -z $yn ]; then
+        rm -Rf $2
+	else
+		echo "Please remove or provide a different output directory."
+		exit -3
+	fi
 fi
 
 BASEDIR=$(dirname "$0")
