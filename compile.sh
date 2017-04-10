34afe6ee30699133011845bcec32088f51f9b29d
Robert Dyer <rdyer@bgsu.edu>
2017-04-10 15:56:08 -0400

    refactoring a bit how argument checking is handled

--- compile.sh
+++ compile.sh
@@ -2,12 +2,14 @@
 
 if [ "$#" -lt 1 ]; then
     echo "Usage: $0 file.boa [options]"
+    $(dirname "$0")/boa.sh -c
     exit -1
 fi
 
 if [ ! -f "$1" ]; then
-    echo "Error: input '$1' is not a file"
+    echo "input '$1' is not a file"
     echo "Usage: $0 file.boa [options]"
+    $(dirname "$0")/boa.sh -c
     exit -2
 fi
 
--- evaluate.sh
+++ evaluate.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
 if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 path/to/input.boa output-dir/ [options]"
+    echo "Usage: $0 file.boa output-dir/ [options]"
+    $(dirname "$0")/boa.sh -e
     exit -1
 fi
 
 if [ ! -f $1 ]; then
-    echo "Error: input '$1' is not a file"
-    echo "Usage: $0 path/to/input.boa output-dir/ [options]"
+    echo "input '$1' is not a file"
+    echo "Usage: $0 file.boa output-dir/ [options]"
+    $(dirname "$0")/boa.sh -e
     exit -2
-fi
-
-if [ -d $2 ]; then
-    read -n 1 -p "output directory '$2' exists - delete? [Y/n] " yn
-    echo ""
-
-    yn=`echo $yn | tr '[:upper:]' '[:lower:]'`
-
-    if [ "$yn" == "y" ] || [ "$yn" == "" ]; then
-        rm -Rf $2
-    else
-        echo "Please remove or provide a different output directory."
-        exit -3
-    fi
 fi
 
 $(dirname "$0")/boa.sh -e -d dataset/ -i $1 -o $2 $*
