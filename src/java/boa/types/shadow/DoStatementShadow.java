e66556e3f701301c7c9e539dd5ed12c46f53eac5
Kaushik <kaushin@bgsu.edu>
2017-05-07 12:59:48 -0400

    added all base statement shadow(some need translation)

---
+++ src/java/boa/types/shadow/DoStatementShadow.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2017, Hridesh Rajan, Robert Dyer, Kaushik Nimmala
+ *                 Iowa State University of Science and Technology
+ *                 and Bowling Green State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package boa.types.shadow;
+
+import boa.compiler.ast.Call;
+import boa.compiler.ast.expressions.Expression;
+import boa.compiler.ast.Factor;
+import boa.compiler.ast.Identifier;
+import boa.compiler.ast.Node;
+import boa.compiler.SymbolTable;
+import boa.compiler.transforms.ASTFactory;
+import boa.types.BoaInt;
+import boa.types.BoaProtoList;
+import boa.types.BoaShadowType;
+import boa.types.proto.enums.StatementKindProtoMap;
+import boa.types.proto.ExpressionProtoTuple;
+import boa.types.proto.StatementProtoTuple;
+
+/**
+ * A shadow type for DoStatement.
+ * 
+ * @author rdyer
+ * @author kaushin
+ */
+public class DoStatementShadow extends BoaShadowType  {
+    /**
+     * Construct a {@link DoStatementShadow}.
+     */
+    public DoStatementShadow() {
+        super(new StatementProtoTuple());
+
+        addShadow("expression", new ExpressionProtoTuple());
+        addShadow("body", new StatementProtoTuple());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Node lookupCodegen(final String name, final String nodeId, final SymbolTable env) {
+        final Identifier id = ASTFactory.createIdentifier(nodeId, env);
+        id.type = new StatementProtoTuple();
+       
+        if ("expression".equals(name)) {
+            // ${0}.expression
+            return ASTFactory.createSelector(id, "expression", new ExpressionProtoTuple(), new ExpressionProtoTuple(), env);
+        }
+
+        if ("body".equals(name)) {
+            // ${0}.statements
+            final Expression tree = ASTFactory.createSelector(id, "statements", new BoaProtoList(new StatementProtoTuple()), new StatementProtoTuple(), env);
+            // ${0}.statements[0]
+            ASTFactory.getFactorFromExp(tree).addOp(ASTFactory.createIndex(ASTFactory.createIntLiteral(0), env));
+
+            return tree;
+        }
+
+        throw new RuntimeException("invalid shadow field: " + name);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Expression getKindExpression(final SymbolTable env) {
+        return getKindExpression("StatementKind", "DO", new StatementKindProtoMap(), env);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString() {
+        return "DoStatement";
+    }
+}
---
+++ src/java/boa/types/shadow/VariableDeclarationStatementShadow.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017, Hridesh Rajan, Robert Dyer, Kaushik Nimmala
+ *                 Iowa State University of Science and Technology
+ *                 and Bowling Green State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package boa.types.shadow;
+
+import boa.compiler.ast.Call;
+import boa.compiler.ast.expressions.Expression;
+import boa.compiler.ast.Factor;
+import boa.compiler.ast.Identifier;
+import boa.compiler.ast.Node;
+import boa.compiler.SymbolTable;
+import boa.compiler.transforms.ASTFactory;
+import boa.types.BoaInt;
+import boa.types.BoaProtoList;
+import boa.types.BoaShadowType;
+import boa.types.proto.enums.StatementKindProtoMap;
+import boa.types.proto.ExpressionProtoTuple;
+import boa.types.proto.StatementProtoTuple;
+
+/**
+ * A shadow type for VariableDeclarationStatement.
+ * 
+ * @author rdyer
+ * @author kaushin
+ */
+public class VariableDeclarationStatementShadow extends BoaShadowType  {
+    /**
+     * Construct a {@link VariableDeclarationStatementShadow}.
+     */
+    public VariableDeclarationStatementShadow() {
+        super(new StatementProtoTuple());
+
+        
+        addShadow("body", new StatementProtoTuple());
+        addShadow("finallyblock", new StatementProtoTuple());
+        addShadow("catchclauses", new StatementProtoTuple());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Node lookupCodegen(final String name, final String nodeId, final SymbolTable env) {
+        final Identifier id = ASTFactory.createIdentifier(nodeId, env);
+        id.type = new StatementProtoTuple();
+
+
+        throw new RuntimeException("invalid shadow field: " + name);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Expression getKindExpression(final SymbolTable env) {
+        return getKindExpression("StatementKind", "OTHER", new StatementKindProtoMap(), env);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString() {
+        return "VariableDeclarationStatement";
+    }
+}
