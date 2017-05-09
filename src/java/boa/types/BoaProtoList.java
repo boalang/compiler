24d4d847067715c94c250c9c24868472f1f3272a
Robert Dyer <rdyer@bgsu.edu>
2017-05-09 11:30:11 -0400

    when checking for comparisons, allow one side to be a function

--- src/java/boa/types/BoaProtoList.java
+++ src/java/boa/types/BoaProtoList.java
@@ -90,6 +90,10 @@ public class BoaProtoList extends BoaType {
 	/** {@inheritDoc} */
 	@Override
 	public boolean compares(final BoaType that) {
+		// if that is a function, check the return type
+		if (that instanceof BoaFunction)
+			return this.compares(((BoaFunction) that).getType());
+
 		// if that is an array..
 		if (that instanceof BoaProtoList)
 			// check against the element types of these arrays
