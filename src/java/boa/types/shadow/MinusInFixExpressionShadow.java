f7babb7ef8dbaedcfedbfc35983868df022a065d
kaushin <kaushin@bgsu.edu>
2017-05-31 07:52:59 -0400

    working on one to many mapping

---
+++ src/java/boa/types/shadow/MinusInFixExpressionShadow.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017, Hridesh Rajan, Robert Dyer, Kaushik Nimmala
+ *                 Iowa State University of Science and Technology
+ *                 and Bowling Green State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package boa.types.shadow;
+
+import boa.compiler.ast.Call;
+import boa.compiler.ast.expressions.Expression;
+import boa.compiler.ast.Factor;
+import boa.compiler.ast.Identifier;
+import boa.compiler.ast.Node;
+import boa.compiler.SymbolTable;
+import boa.compiler.transforms.ASTFactory;
+import boa.types.BoaInt;
+import boa.types.BoaProtoList;
+import boa.types.BoaShadowType;
+import boa.types.proto.enums.ExpressionKindProtoMap;
+import boa.types.proto.ExpressionProtoTuple;
+import boa.types.proto.StatementProtoTuple;
+import boa.types.proto.TypeProtoTuple;
+import boa.types.shadow.InfixExpressionShadow;
+/**
+ * A shadow type for MinusInfixExpression.
+ * 
+ * @author rdyer
+ * @author kaushin
+ */
+public class MinusInFixExpressionShadow extends InfixExpressionShadow  {
+    /**
+     * Construct a {@link MinusInfixExpressionShadow}.
+     */
+    
+    /** {@inheritDoc} */
+    @Override
+    public Expression getKindExpression(final SymbolTable env) {
+        return getKindExpression("ExpressionKind", "OP_SUB", new ExpressionKindProtoMap(), env);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString() {
+        return "MinusInfixExpression";
+    }
+}
