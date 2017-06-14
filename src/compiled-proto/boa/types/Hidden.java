425c0d4d292721a72264c39ccb4581862c6652b0
Robert Dyer <rdyer@bgsu.edu>
2017-06-14 13:40:12 -0400

    add kind_func proto extension option

--- src/compiled-proto/boa/types/Hidden.java
+++ src/compiled-proto/boa/types/Hidden.java
@@ -8,6 +8,7 @@ public final class Hidden {
   public static void registerAllExtensions(
       com.google.protobuf.ExtensionRegistry registry) {
     registry.add(boa.types.Hidden.hidden);
+    registry.add(boa.types.Hidden.kindFunc);
   }
   public static final int HIDDEN_FIELD_NUMBER = 50000;
   /**
@@ -20,6 +21,17 @@ public final class Hidden {
           .newFileScopedGeneratedExtension(
         java.lang.Boolean.class,
         null);
+  public static final int KIND_FUNC_FIELD_NUMBER = 50001;
+  /**
+   * <code>extend .google.protobuf.MessageOptions { ... }</code>
+   */
+  public static final
+    com.google.protobuf.GeneratedMessage.GeneratedExtension<
+      com.google.protobuf.DescriptorProtos.MessageOptions,
+      java.lang.String> kindFunc = com.google.protobuf.GeneratedMessage
+          .newFileScopedGeneratedExtension(
+        java.lang.String.class,
+        null);
 
   public static com.google.protobuf.Descriptors.FileDescriptor
       getDescriptor() {
@@ -31,7 +43,9 @@ public final class Hidden {
     java.lang.String[] descriptorData = {
       "\n\014hidden.proto\022\tboa.types\032 google/protob" +
       "uf/descriptor.proto:6\n\006hidden\022\035.google.p" +
-      "rotobuf.FieldOptions\030\320\206\003 \001(\010:\005falseB\002H\001"
+      "rotobuf.FieldOptions\030\320\206\003 \001(\010:\005false:6\n\tk" +
+      "ind_func\022\037.google.protobuf.MessageOption" +
+      "s\030\321\206\003 \001(\t:\000B\002H\001"
     };
     com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
       new com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner() {
@@ -39,6 +53,7 @@ public final class Hidden {
             com.google.protobuf.Descriptors.FileDescriptor root) {
           descriptor = root;
           hidden.internalInit(descriptor.getExtensions().get(0));
+          kindFunc.internalInit(descriptor.getExtensions().get(1));
           return null;
         }
       };
