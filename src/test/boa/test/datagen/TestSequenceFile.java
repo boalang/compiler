d2421db339e7505284fea66f39f642dbfb4289f6
nguyen_hoan <hoan@iastate.edu>
2017-07-17 02:24:39 -0500

    add utilities for testing generated sequence files

--- src/test/boa/test/datagen/TestSequenceFile.java
+++ src/test/boa/test/datagen/TestSequenceFile.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.hamcrest.*;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -17,23 +21,36 @@ import org.apache.hadoop.io.WritableComparable;
 import org.junit.Test;
 
 import com.google.protobuf.CodedInputStream;
+import com.google.protobuf.Message;
+import com.google.protobuf.Descriptors.FieldDescriptor;
+import com.google.protobuf.Descriptors.FieldDescriptor.Type;
 import com.googlecode.protobuf.format.JsonFormat;
 
+import boa.datagen.util.ProtoMessageVisitor;
 import boa.types.Ast.ASTRoot;
+import boa.types.Ast.Declaration;
 import boa.types.Code.CodeRepository;
 import boa.types.Diff.ChangedFile;
 import boa.types.Toplevel.Project;
 
 public class TestSequenceFile {
-
+	private Configuration conf = new Configuration();
+	private FileSystem fileSystem;
+	private SequenceFile.Reader pr;
+	private SequenceFile.Reader ar;
+	private Declaration decl = null;
+		
+	public TestSequenceFile() throws IOException {
+		fileSystem = FileSystem.get(conf);
+		pr = new SequenceFile.Reader(fileSystem, new Path("dataset/projects.seq"), conf);
+		ar = new SequenceFile.Reader(fileSystem, new Path("dataset/data"), conf);
+	}
+	
 	@Test
 	public void astPrint() throws IOException{
-		Configuration conf = new Configuration();
-		FileSystem fileSystem = FileSystem.get(conf);
 		Writable key = new LongWritable();
 	//	Writable key = new Text();
 		BytesWritable val = new BytesWritable();
-		SequenceFile.Reader ar = new SequenceFile.Reader(fileSystem, new Path("dataset/data"), conf);
 		//WritableComparable key = (WritableComparable) r.getKeyClass().newInstance();
 		//System.out.println("Key class is: " + key.getClass().getName());
 		while (ar.next(key, val)) {
@@ -45,17 +62,14 @@ public class TestSequenceFile {
 			ASTRoot root = ASTRoot.parseFrom(CodedInputStream.newInstance(bytes, 0, val.getLength()));
 			System.out.println(root);
 		}
+		pr.close();
 		ar.close();
 	}
 	
 	@Test
-	public void projectSeqTest() throws IOException{
-		Configuration conf = new Configuration();
-		FileSystem fileSystem = FileSystem.get(conf);
+	public void projectSeqTest() throws IOException {
 		Writable key = new Text();
 		BytesWritable val = new BytesWritable();
-		SequenceFile.Reader pr = new SequenceFile.Reader(fileSystem, new Path("dataset/projects.seq"), conf);
-		SequenceFile.Reader ar = new SequenceFile.Reader(fileSystem, new Path("dataset/data"), conf);
 		while (pr.next(key, val)) {
 			byte[] bytes = val.getBytes();
 			Project project = Project.parseFrom(CodedInputStream.newInstance(bytes, 0, val.getLength()));
@@ -76,8 +90,10 @@ public class TestSequenceFile {
 			System.out.println(project.getHomepageUrl());
 			System.out.println(project.getProjectUrl());
 			System.out.println(project.getDescription() + "\n");
-			CodeRepository cr = project.getCodeRepositories(0);
+			final CodeRepository cr = project.getCodeRepositories(0);
 			assertTrue(cr.getHeadSnapshotCount() > 0);
+//			HashMap<Integer, HashMap<Integer, Declaration>> fileNodeDeclaration = collectDeclarations(cr.getHeadSnapshotList());
+			final HashMap<Integer, HashMap<Integer, Declaration>> fileNodeDeclaration = new HashMap<Integer, HashMap<Integer, Declaration>>();
 			for (ChangedFile cf : cr.getHeadSnapshotList()) {
 				long astpos = cf.getKey();
 				if (astpos > -1) {
@@ -87,11 +103,113 @@ public class TestSequenceFile {
 					ar.next(astkey, val);
 					bytes = val.getBytes();
 					ASTRoot root = ASTRoot.parseFrom(CodedInputStream.newInstance(bytes, 0, val.getLength()));
-					System.out.println(root);
+//					System.out.println(root);
+					ProtoMessageVisitor v = new ProtoMessageVisitor() {
+						@Override
+						public boolean preVisit(Message message) {
+							if (message instanceof boa.types.Ast.Type) {
+								boa.types.Ast.Type type = (boa.types.Ast.Type) message;
+								String fqn = type.getFullyQualifiedName();
+								final int fileId = type.getDeclarationFile(), nodeId = type.getDeclaration();
+								if (fqn != null && !fqn.isEmpty() && fileId > 0) {
+									decl = null;
+									HashMap<Integer, Declaration> declarations = fileNodeDeclaration.get(fileId);
+									if (declarations != null) {
+										decl = declarations.get(nodeId);
+									} else {
+										declarations = new HashMap<Integer, Declaration>();
+										fileNodeDeclaration.put(fileId, declarations);
+									}
+									if (decl == null) {
+										ChangedFile dcf = cr.getHeadSnapshot(fileId);
+										decl = getDeclaration(dcf, nodeId);
+									}
+									if (decl == null || !fqn.equals(decl.getFullyQualifiedName()))
+										System.out.println(fqn);
+									assertEquals(true, decl != null && fqn.equals(decl.getFullyQualifiedName()));
+									declarations.put(nodeId, decl);
+								}
+							}
+							return true;
+						}
+
+						private Declaration getDeclaration(final ChangedFile cf, final int nodeId) {
+							long astpos = cf.getKey();
+							if (astpos > -1) {
+								try {
+									ar.seek(astpos);
+									Writable astkey = new LongWritable();
+									BytesWritable val = new BytesWritable();
+									ar.next(astkey, val);
+									byte[] bytes = val.getBytes();
+									ASTRoot root = ASTRoot.parseFrom(CodedInputStream.newInstance(bytes, 0, val.getLength()));
+									ProtoMessageVisitor v = new ProtoMessageVisitor() {
+										@Override
+										public boolean preVisit(Message message) {
+											if (decl != null)
+												return false;
+											if (message instanceof Declaration) {
+												Declaration type = (Declaration) message;
+												if (type.getKey() == nodeId)
+													decl = type;
+												return false;
+											}
+											return true;
+										};
+									};
+									v.visit(root);
+								} catch (IOException e) {}
+							}
+							return decl;
+						}
+					};
+					v.visit(root);
 				}
 			}
 		}
 		pr.close();
 		ar.close();
+	}
+
+	private HashMap<Integer, HashMap<Integer, Declaration>> collectDeclarations(List<ChangedFile> snapshot) throws IOException {
+		HashMap<Integer, HashMap<Integer, Declaration>> fileNodeDeclaration = new HashMap<Integer, HashMap<Integer, Declaration>>();
+		for (int fileIndex = 0; fileIndex < snapshot.size(); fileIndex++) {
+			ChangedFile cf = snapshot.get(fileIndex);
+			long astpos = cf.getKey();
+			if (astpos > -1) {
+				ar.seek(astpos);
+				Writable astkey = new LongWritable();
+				BytesWritable val = new BytesWritable();
+				ar.next(astkey, val);
+				byte[] bytes = val.getBytes();
+				ASTRoot root = ASTRoot.parseFrom(CodedInputStream.newInstance(bytes, 0, val.getLength()));
+				HashMap<Integer, Declaration> nodeDeclaration = collectDeclarations(root);
+				fileNodeDeclaration.put(fileIndex, nodeDeclaration);
+			}
+		}
+		return fileNodeDeclaration;
+	}
+
+	private HashMap<Integer, Declaration> collectDeclarations(Message message) {
+		HashMap<Integer, Declaration> nodeDeclaration = new HashMap<Integer, Declaration>();
+		if (message instanceof Declaration) {
+			nodeDeclaration.put(((Declaration) message).getKey(), (Declaration) message);
+		}
+		for (Iterator<Map.Entry<FieldDescriptor, Object>> iter = message.getAllFields().entrySet().iterator(); iter.hasNext();) {
+            Map.Entry<FieldDescriptor, Object> field = iter.next();
+            nodeDeclaration.putAll(collectDeclarations(field.getKey(), field.getValue()));
+        }
+		return nodeDeclaration;
+	}
+
+	private HashMap<Integer, Declaration> collectDeclarations(FieldDescriptor field, Object value) {
+		HashMap<Integer, Declaration> nodeDeclaration = new HashMap<Integer, Declaration>();
+        if (field.isRepeated()) {
+            // Repeated field. Print each element.
+            for (Iterator<?> iter = ((List<?>) value).iterator(); iter.hasNext();)
+            	if (field.getType() == Type.MESSAGE)
+            		nodeDeclaration.putAll(collectDeclarations((Message) iter.next()));
+        }
+		return nodeDeclaration;
 	}
 }
