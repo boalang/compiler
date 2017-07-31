585cb34662c09bbec9401f425b857c263ef65bae
robert2 <robert2@iastate.edu>
2017-07-31 10:44:31 -0500

    added tests for throw and try catch

---
+++ src/test/boa/test/datagen/TestTryCatch.java
@@ -0,0 +1,14 @@
+package boa.test.datagen;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class TestTryCatch extends JavaScriptBaseTest {
+
+	@Test
+	public void TryCatchTest1() throws IOException {
+		nodeTest(load("test/datagen/javascript/TryCatchNodes.boa"), load("test/datagen/javascript/TryCatchNodes.js"));
+	}
+
+}
