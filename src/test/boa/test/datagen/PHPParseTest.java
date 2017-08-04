b66a456a6b09f0b2c7cebf25754e0903d2d0d1fc
robert2 <robert2@iastate.edu>
2017-08-04 08:07:42 -0500

    minor

--- src/test/boa/test/datagen/PHPParseTest.java
+++ src/test/boa/test/datagen/PHPParseTest.java
@@ -1,5 +1,7 @@
 package boa.test.datagen;
 
+import static org.junit.Assert.*;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -10,18 +12,22 @@ import org.junit.Test;
 //import org.eclipse.dltk.core.ModelException;
 
 import boa.datagen.util.FileIO;
+import boa.datagen.util.PHPErrorCheckVisitor;
 import boa.datagen.util.PHPVisitor;
+import boa.types.Ast.Namespace;
 
 public class PHPParseTest {
 	
 	@Test
 	public void parseTest() throws Exception{
-		ASTParser parser = org.eclipse.php.internal.core.ast.nodes.ASTParser.newParser(PHPVersion.PHP5);
+		ASTParser parser = org.eclipse.php.internal.core.ast.nodes.ASTParser.newParser(PHPVersion.PHP7_1);
 		String content = FileIO.readFileContents(new File("test/datagen/PHP/PHPParseTest.php"));
 		parser.setSource(content.toCharArray());
 		Program ast = parser.createAST(null);
 		PHPVisitor visitor = new PHPVisitor(content);
-		boa.types.Ast.ASTRoot root = visitor.getRoot(ast);
+		PHPErrorCheckVisitor errorCheck = new PHPErrorCheckVisitor();
+		assertFalse(errorCheck.hasError);
+		Namespace root = visitor.getNamespace(ast);
 		System.out.println(root);
 	}
 }
