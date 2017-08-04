6f86e4a0b312a8c87cf4324bdaae76ad43327604
robert2 <robert2@iastate.edu>
2017-08-04 10:59:50 -0500

    Store cached issue data as a .seq

--- src/java/boa/datagen/CacheGithubIssues.java
+++ src/java/boa/datagen/CacheGithubIssues.java
@@ -1,7 +1,17 @@
 package boa.datagen;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -15,10 +25,27 @@ public class CacheGithubIssues {
 	final static String jsonPath = Properties.getProperty("gh.issue.path", DefaultProperties.GH_ISSUE_PATH);
 	final static String jsonCachePath = Properties.getProperty("gh.json.cache.path",
 			DefaultProperties.GH_JSON_CACHE_PATH);
-
+	private static Configuration conf;
+	private static FileSystem fileSystem;
+	private static String suffix = "";
+	private static SequenceFile.Writer issueWriter;
+	private static String outPath; 
+	
 	public static void main(String[] args) {
+		outPath = args[1]; //jsonCachePath
+		conf = new Configuration();
+		try {
+			fileSystem = FileSystem.get(conf);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		File output = new File(args[1]); //jsonCachePath);
+		if (!output.exists())
+			output.mkdirs();
+		CacheGithubIssues.openWriters();
 		HashMap<String, byte[]> repos = new HashMap<String, byte[]>();
-		File dir = new File(jsonPath + "/repos");
+		File dir = new File(args[0]); //jsonPath + "/issues");
 		for (File file : dir.listFiles()) {
 			if (file.getName().endsWith(".json")) {
 				String content = FileIO.readFileContents(file);
@@ -30,13 +57,53 @@ public class CacheGithubIssues {
 						Issue protobufRepo = repo.toBoaMetaDataProtobuf();
 						// System.out.println(jRepo.toString());
 						repos.put(repo.id, protobufRepo.toByteArray());
-						System.out.println(repos.size() + ": " + repo.id);
+						try {
+							issueWriter.append(new Text(repo.id), new BytesWritable(protobufRepo.toByteArray()));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
 					}
+					System.out.println(repos.size() + ": " + file.getName());
 				}
 			}
 		}
-		File output = new File(jsonCachePath);
-		output.mkdirs();
-		FileIO.writeObjectToFile(repos, jsonCachePath + "/issue-buf-map" , false);
+		CacheGithubIssues.closeWriters();
 	}
+	private static void openWriters() {
+		long time = System.currentTimeMillis() / 1000;
+		suffix = "-" + time + ".seq";
+		while (true) {
+			try {
+				issueWriter = SequenceFile.createWriter(fileSystem, conf, new Path(outPath + "/issues" + suffix), Text.class, BytesWritable.class);
+				break;
+			} catch (Throwable t) {
+				t.printStackTrace();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {}
+			}
+		}
+	}
+	
+	private static void closeWriters() {
+		while (true) {
+			try {
+				issueWriter.close();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+				}
+				fileSystem.delete(new Path(new Path(outPath), "." + "issues" + suffix + ".crc"), false);
+				break;
+			} catch (Throwable t) {
+				t.printStackTrace();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+	}
+
 }
+
