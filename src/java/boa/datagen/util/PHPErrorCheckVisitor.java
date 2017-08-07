dafd31a6825b48481c308d1e5ff1a11aed5e23e5
robert2 <robert2@iastate.edu>
2017-08-07 15:25:07 -0500

    Use ApplyAll instead of AbstractVisitor

--- src/java/boa/datagen/util/PHPErrorCheckVisitor.java
+++ src/java/boa/datagen/util/PHPErrorCheckVisitor.java
@@ -1,15 +1,17 @@
 package boa.datagen.util;
 
-import org.eclipse.php.internal.core.ast.nodes.ASTError;
+import org.eclipse.php.internal.core.ast.nodes.*;
 import org.eclipse.php.internal.core.ast.nodes.ASTNode;
 import org.eclipse.php.internal.core.ast.visitor.AbstractVisitor;
+import org.eclipse.php.internal.core.ast.visitor.ApplyAll;
 
-public class PHPErrorCheckVisitor extends AbstractVisitor{
+public class PHPErrorCheckVisitor extends ApplyAll{
 	public boolean hasError = false;
 	
+
 	@Override
-	public boolean visit(ASTNode node){
-		if ((node.getFlags() & ASTNode.MALFORMED) != 0 || (node.getFlags() & ASTNode.RECOVERED) != 0 || (node instanceof ASTError))
+	protected boolean apply(ASTNode node) {
+		if (node == null || (node instanceof ASTError) || (node.getFlags() & ASTNode.MALFORMED) != 0 || (node.getFlags() & ASTNode.RECOVERED) != 0 || (node instanceof ASTError))
 			hasError = true;
 		return !hasError;
 	}
