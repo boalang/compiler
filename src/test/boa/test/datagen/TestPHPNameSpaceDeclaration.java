751f5e3ecfd890e5bfc2c7270644d9b8f88c503a
robert2 <robert2@iastate.edu>
2017-08-08 15:43:36 -0500

    NamespacesDeclarations don't have to have brackets.

--- src/test/boa/test/datagen/TestPHPNameSpaceDeclaration.java
+++ src/test/boa/test/datagen/TestPHPNameSpaceDeclaration.java
@@ -13,6 +13,12 @@ public class TestPHPNameSpaceDeclaration extends PHPBaseTest {
 	}
 	
 	@Test
+	public void testNamespaceDeclaration2() throws IOException, Exception{
+		nodeTest(load("test/datagen/PHP/NamespaceDeclarationNode2.boa"),
+				 load("test/datagen/PHP/NamespaceDeclarationNode2.php"));
+	}
+	
+	@Test
 	public void testNestedNamespaceDeclarations() throws IOException, Exception{
 		nodeTest(load("test/datagen/PHP/NestedNamespaces.boa"),
 				 load("test/datagen/PHP/NestedNamespaces.php"));
