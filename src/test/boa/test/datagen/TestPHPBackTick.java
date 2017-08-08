97fd693239996a4b77b00694843a433be8dc9b4a
robert2 <robert2@iastate.edu>
2017-08-08 15:51:57 -0500

    Test for BackTick node

---
+++ src/test/boa/test/datagen/TestPHPBackTick.java
@@ -0,0 +1,15 @@
+package boa.test.datagen;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class TestPHPBackTick extends PHPBaseTest {
+
+	@Test
+	public void testCBackTick() throws IOException, Exception{
+		nodeTest(load("test/datagen/PHP/BackTickNode.boa"),
+				load("test/datagen/PHP/BackTickNode.php"));
+	}
+	
+}
