be4997a12a6efb9bb9849d2fc545ca7251c27e42
robert2 <robert2@iastate.edu>
2017-08-08 14:25:30 -0500

    Added tests for Function, Method, Lambda declarations.

---
+++ src/test/boa/test/datagen/TestPHPFunctionDeclaration.java
@@ -0,0 +1,27 @@
+package boa.test.datagen;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class TestPHPFunctionDeclaration extends PHPBaseTest {
+
+	@Test
+	public void testFunctionDeclaration() throws IOException, Exception{
+		nodeTest(load("test/datagen/PHP/FunctionDeclarationNode.boa"),
+				load("test/datagen/PHP/FunctionDeclarationNode.php"));
+	}
+	
+	@Test
+	public void testLambdaDeclaration() throws IOException, Exception{
+		nodeTest(load("test/datagen/PHP/LambdaDeclarationNode.boa"),
+				load("test/datagen/PHP/LambdaDeclarationNode.php"));
+	}
+	
+	@Test
+	public void testMethodDeclaration() throws IOException, Exception{
+		nodeTest(load("test/datagen/PHP/MethodDeclarationNode.boa"),
+				load("test/datagen/PHP/MethodDeclarationNode.php"));
+	}
+	
+}
