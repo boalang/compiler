c6227da1b2cfcbcc72f3e928d8990bb81c00e50a
robert2 <robert2@iastate.edu>
2017-10-03 09:41:06 -0500

    Added Java Node Tests.

--- src/test/boa/test/datagen/java/TestAssignment.java
+++ src/test/boa/test/datagen/java/TestAssignment.java
@@ -15,6 +15,46 @@ public class TestAssignment extends Java8BaseTest {
 	}
 	
 	@Test
+	public void bitAndAssignment() throws IOException {
+		testWrapped(
+			load("test/datagen/java/BitAndAssignment.java").trim(),
+			load("test/datagen/boa/BitAndAssignment.boa").trim()
+		);
+	}
+	
+	@Test
+	public void bitOrAssignment() throws IOException {
+		testWrapped(
+			load("test/datagen/java/BitOrAssignment.java").trim(),
+			load("test/datagen/boa/BitOrAssignment.boa").trim()
+		);
+	}
+	
+	@Test
+	public void bitXOrAssignment() throws IOException {
+		testWrapped(
+			load("test/datagen/java/BitXorAssignment.java").trim(),
+			load("test/datagen/boa/BitXorAssignment.boa").trim()
+		);
+	}
+	
+	@Test
+	public void bitDivideAssignment() throws IOException {
+		testWrapped(
+			load("test/datagen/java/DivideAssignment.java").trim(),
+			load("test/datagen/boa/DivideAssignment.boa").trim()
+		);
+	}
+	
+	@Test
+	public void lShiftAssignment() throws IOException {
+		testWrapped(
+			load("test/datagen/java/LeftShiftAssignment.java").trim(),
+			load("test/datagen/boa/LeftShiftAssignment.boa").trim()
+		);
+	}
+	
+	@Test
 	public void plusAssignment() throws IOException {
 		testWrapped(
 			load("test/datagen/java/PlusAssignment.java").trim(),
@@ -27,6 +67,38 @@ public class TestAssignment extends Java8BaseTest {
 		testWrapped(
 			load("test/datagen/java/MinusAssignment.java").trim(),
 			load("test/datagen/boa/MinusAssignment.boa").trim()
+		);
+	}
+	
+	@Test
+	public void modAssignment() throws IOException {
+		testWrapped(
+			load("test/datagen/java/ModAssignment.java").trim(),
+			load("test/datagen/boa/ModAssignment.boa").trim()
+		);
+	}
+	
+	@Test
+	public void rShiftAssignment() throws IOException {
+		testWrapped(
+			load("test/datagen/java/RightShiftSignedAssignment.java").trim(),
+			load("test/datagen/boa/RightShiftSignedAssignment.boa").trim()
+		);
+	}
+	
+	@Test
+	public void UnsignedRShiftAssignment() throws IOException {
+		testWrapped(
+			load("test/datagen/java/RightShiftUnsignedAssignment.java").trim(),
+			load("test/datagen/boa/RightShiftUnsignedAssignment.boa").trim()
+		);
+	}
+	
+	@Test
+	public void TimesAssignment() throws IOException {
+		testWrapped(
+			load("test/datagen/java/TimesAssignment.java").trim(),
+			load("test/datagen/boa/TimesAssignment.boa").trim()
 		);
 	}
 	
---
+++ test/datagen/java/ClassInstance.java
@@ -0,0 +1,1 @@
+Person x = new Person();
