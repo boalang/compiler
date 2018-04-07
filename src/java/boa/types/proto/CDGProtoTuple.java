df0a2c0e807e6916dd5c52896a91dbd5805abcfd
arafat1 <marafat@bgsu.edu>
2018-04-07 11:38:37 -0400

    add types for cdg, cdgnode and cdgedge

---
+++ src/java/boa/types/proto/CDGProtoTuple.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2018, Robert Dyer, Mohd Arafat
+ *                 and Bowling Green State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package boa.types.proto;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import boa.types.BoaProtoTuple;
+import boa.types.BoaString;
+import boa.types.BoaType;
+import boa.types.BoaSet;
+
+/**
+ * A {@link CDGProtoTuple}.
+ *
+ * @author marafat
+ */
+public class CDGProtoTuple extends BoaProtoTuple {
+
+    private final static List<BoaType> members = new ArrayList<BoaType>();
+    private final static Map<String, Integer> names = new HashMap<String, Integer>();
+
+    static {
+        int counter = 0;
+
+        names.put("md", counter++);
+        members.add(new MethodProtoTuple());
+
+        names.put("class_name", counter++);
+        members.add(new BoaString());
+
+        names.put("nodes", counter++);
+        members.add(new BoaSet(new CDGNodeProtoTuple()));
+
+    }
+
+    /**
+     * Construct a {@link CDGProtoTuple}.
+     */
+    public CDGProtoTuple() {
+        super(members, names);
+    }
+
+    /** @{inheritDoc} */
+    @Override
+    public String toJavaType() {
+        return "boa.graphs.cdg.CDG";
+    }
+
+}
