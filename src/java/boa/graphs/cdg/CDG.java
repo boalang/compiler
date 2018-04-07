d27345ed87566339378e91dd4ae38fc195ff601f
arafat1 <marafat@bgsu.edu>
2018-04-06 21:52:30 -0400

    finish CDG construction

--- src/java/boa/graphs/cdg/CDG.java
+++ src/java/boa/graphs/cdg/CDG.java
@@ -1,89 +1,104 @@
 package boa.graphs.cdg;
 
-
-import boa.functions.BoaIntrinsics;
 import boa.graphs.cfg.CFG;
+import boa.graphs.cfg.CFGEdge;
 import boa.graphs.cfg.CFGNode;
-import boa.runtime.BoaAbstractFixP;
-import boa.runtime.BoaAbstractTraversal;
 import boa.types.Ast.*;
-import boa.types.Graph;
+import boa.types.Control;
 
 import java.util.*;
 
 public class CDG {
 
+    private Method md;
+    private String class_name;
+    private CDGNode entryNode;
+    private Set<CDGNode> nodes = new HashSet<CDGNode>();
+
+    public CDG(final CFG cfg) throws Exception {
+        this.md = cfg.md;
+        this.class_name = cfg.class_name;
+        PDTree pdTree = new PDTree(cfg);
+        constructCDG(pdTree, cfg);
+    }
+
     public CDG(final Method method) throws Exception {
-        CFG cfg = new CFG(method);
-        Map<Integer, Set<CFGNode>> pdomTree = postDominator(cfg);
-        //dominatorFrontier();
-        //mapEdges();
+        this(new CFG(method));
+    }
+
+    //Getters
+    private Method getMd() {
+        return md;
+    }
+
+    private String getClass_name() {
+        return class_name;
+    }
+
+    public CDGNode getEntryNode() {
+        return entryNode;
+    }
+
+    public Set<CDGNode> getNodes() {
+        return nodes;
     }
 
     /**
-     * Constructs a post-dominator tree for the given control flow graph
+     * Constructs a Control Dependence Graph for a given post dominator tree
      *
+     * @param pdTree post dominator tree
      * @param cfg control flow graph
-     * @return
-     * @throws Exception
      */
-    private Map<Integer, Set<CFGNode>> postDominator(final CFG cfg) throws Exception {
-        final BoaAbstractTraversal pDom = new BoaAbstractTraversal<HashSet<CFGNode>>(true, true) {
+    private void constructCDG(PDTree pdTree, CFG cfg) {
+        Map<Integer[], String> controlEdges = new HashMap<Integer[], String>();
+        for (CFGNode n: cfg.getNodes()) {
+            if (n.getKind() == Control.CFGNode.CFGNodeType.CONTROL)
+            for (CFGEdge e: n.outEdges)
+                controlEdges.put(new Integer[]{e.getSrc().getId(), e.getDest().getId()}, e.label());
+        }
+        controlEdges.put(new Integer[]{cfg.getNodes().size(), 0}, "T");
 
-            protected Set<CFGNode> preTraverse(final CFGNode node) throws Exception {
-                Set<CFGNode> currentPDom = new HashSet<CFGNode>();
+        int graphSize = pdTree.getNodes().size();
 
-                if (node.getId() !=  (cfg.getNodes().size() - 1))
-                    currentPDom =  cfg.getNodes() ;
+        for (Integer[] enodes: controlEdges.keySet()) {
+            TreeNode src = pdTree.getNode(enodes[0]);
+            TreeNode dest = pdTree.getNode(enodes[1]);
+            TreeNode srcParent = pdTree.getNode(enodes[0]).getParent();
+            CDGNode source = new CDGNode(src);
 
-                if ((getValue(node) != null))
-                    currentPDom = getValue(node);
+            while (!srcParent.equals(dest)) {
+                CDGNode destination = getNode(dest);
+                source.addSuccessor(destination);
+                destination.addPredecessor(source);
 
-                for (CFGNode successor: node.getSuccessorsList()) {
-                    if (successor != null) {
-                        Set<CFGNode> succPDom = getValue(successor);
-                        if (succPDom != null)
-                            currentPDom.retainAll(succPDom);
-                    }
-                }
+                CDGEdge edge = new CDGEdge(source, destination, controlEdges.get(enodes));
+                source.addOutEdges(edge);
+                destination.addInEdges(edge);
 
-                if (node != null)
-                    currentPDom.add(node);
+                dest = dest.getParent();
 
-                return currentPDom;
+                if (source.getId() == graphSize-1)
+                    entryNode = source;
             }
-
-            @Override
-            public void traverse(final CFGNode node, boolean flag) throws Exception {
-                if(flag) {
-                    currentResult = (HashSet<CFGNode>)preTraverse(node);
-                    outputMapObj.put(node.getId(), currentResult);
-                }
-                else
-                    outputMapObj.put(node.getId(), (HashSet<CFGNode>)preTraverse(node));
-            }
-
-        };
-
-        BoaAbstractFixP fixp = new BoaAbstractFixP()
-        {
-             boolean invoke1(final HashSet<CFGNode> current, final HashSet<CFGNode> previous) throws Exception {
-                 Set<CFGNode> curr = new HashSet<CFGNode>(current);
-                 curr.removeAll(previous);
-                 if (curr.size() == 0)
-                     return  true ;
-                 return  false ;
-            }
-
-            @Override
-            public boolean invoke(Object current, Object previous) throws Exception{
-                return invoke1((HashSet<CFGNode>)current, (HashSet<CFGNode>)previous);
-            }
-        };
-
-        pDom.traverse( cfg , Graph.Traversal.TraversalDirection.FORWARD, Graph.Traversal.TraversalKind.REVERSEPOSTORDER, fixp);
-
-        return pDom.outputMapObj;
+        }
     }
 
+    /**
+     * Checks if a node already exists and returns it, otherwise returns a new node.
+     *
+     * @param treeNode a post dominator tree node
+     * @return a new tree node or an existing tree node
+     */
+    private CDGNode getNode(TreeNode treeNode) {
+        CDGNode node = new CDGNode(treeNode);
+        if (nodes.contains(node)) {
+            for (CDGNode n : nodes) {
+                if (n == node)
+                    return n;
+            }
+        }
+        nodes.add(node);
+
+        return node;
+    }
 }
