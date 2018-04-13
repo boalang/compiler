9758d1236f6a72f3b142750313326840f322e239
arafat1 <marafat@bgsu.edu>
2018-04-13 12:49:48 -0400

    add license info

--- src/java/boa/graphs/trees/DTree.java
+++ src/java/boa/graphs/trees/DTree.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018, Robert Dyer, Mohd Arafat
+ *                 and Bowling Green State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package boa.graphs.trees;
 
 import java.util.HashMap;
@@ -11,6 +27,10 @@ import boa.graphs.cfg.CFGNode;
 import boa.runtime.BoaAbstractFixP;
 import boa.runtime.BoaAbstractTraversal;
 import boa.types.Graph;
+
+/**
+ * @author marafat
+ */
 
 public class DTree {
 
