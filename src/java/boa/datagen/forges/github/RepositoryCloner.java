34fa9391bbbe08209c9609857e027ce7b54f334b
robert2 <robert2@iastate.edu>
2018-05-24 12:52:29 -0500

    use exponential backoff when there is a exception during cloning

--- src/java/boa/datagen/forges/github/RepositoryCloner.java
+++ src/java/boa/datagen/forges/github/RepositoryCloner.java
@@ -2,12 +2,12 @@ package boa.datagen.forges.github;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
-
 
 /**
  * Simple snippet which shows how to clone a repository from a remote source
@@ -24,22 +24,42 @@ public class RepositoryCloner {
 		File localGitDir = new File(localPath + "/.git");
 		// then clone
 		Git result = null;
-		
-	
-		java.lang.System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
-		try {
-			result = Git.cloneRepository().setURI(url).setBare(true).setDirectory(localGitDir).call();
-			// Note: the call() returns an opened repository already which needs
-			// to be closed to avoid file handle leaks!
-			// workaround for
-			// https://bugs.eclipse.org/bugs/show_bug.cgi?id=474093
-			result.getRepository().close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (result != null && result.getRepository() != null)
-				result.getRepository().close();
-		}
-	}
+		int low = 1;
+		int high = 1000;
+		Random r = new Random();
+		int pow = 1;
 
+		java.lang.System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
+		boolean success = false;
+		int trys = 0;
+		while (success)
+			try {
+				success = true;
+				result = Git.cloneRepository().setURI(url).setBare(true).setDirectory(localGitDir).call();
+				// Note: the call() returns an opened repository already which
+				// needs
+				// to be closed to avoid file handle leaks!
+				// workaround for
+				// https://bugs.eclipse.org/bugs/show_bug.cgi?id=474093
+				result.getRepository().close();
+			} catch (Exception e) {
+				if (e.getMessage().contains("Authentication is required but no CredentialsProvider")) {
+					continue;
+				}
+				if (trys < 5) {
+					try {
+						pow *=2;
+						Thread.sleep(((int) Math.round(pow * 1000) + (r.nextInt(high - low) + low)));
+						success = false;
+					} catch (InterruptedException ie) {
+						// ie.printStackTrace();
+					}
+				}
+				// e.printStackTrace();
+			} finally {
+
+				if (result != null && result.getRepository() != null)
+					result.getRepository().close();
+			}
+	}
 }
