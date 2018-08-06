d4b780ebb43152973161af6f92d95b0652b67a4b
robert2 <robert2@iastate.edu>
2018-08-06 14:30:23 -0500

    Test q20.boa query

--- src/test/boa/test/datagen/TestQueries.java
+++ src/test/boa/test/datagen/TestQueries.java
@@ -15,28 +15,52 @@ import java.io.IOException;
 
 
 public class TestQueries {
+
+	@Test
+	public void testBugFix() {
+		String[] args = { "-i", "/Users/roberts/git/compiler/test/known-good/bug-fix.boa", 
+				"-d", "/Users/roberts/git/compiler/test/datagen/test_datagen",
+				"-o", "/Users/roberts/git/compiler/test/datagen/Bug-fix_output" };
+		BoaEvaluator.main(args);
+		String expected = "AddedNullCheck[] = 1\n";
+		File outputDir = new File("/Users/roberts/git/compiler/test/datagen/Bug-fix_output");
+		String actual = getResults(outputDir);// evaluator.getResults();
+		try {
+			FileUtils.deleteDirectory(outputDir);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(expected, actual);
+	}
 	
 	@Test
-	public void testNullCheck() {
-		
-	String[] args = { "-i",  "/Users/roberts/git/compiler/test/known-good/bug-fix.boa", 
-					  "-d", "/Users/roberts/git/compiler/test/datagen/test_datagen",
-					  "-o", "/Users/roberts/git/compiler/test/datagen/output"};	
-	BoaEvaluator.main(args);	
-	String expected = "AddedNullCheck[] = 1\n";
-	File outputDir = new File( "/Users/roberts/git/compiler/test/datagen/output");
-	String actual =  getResults(outputDir);// evaluator.getResults();
-	try {
-		FileUtils.deleteDirectory(outputDir);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public void testq20() {
+		String[] args = { "-i", "/Users/roberts/git/compiler/test/known-good/q20.boa", 
+				"-d","/Users/roberts/git/compiler/test/datagen/test_datagen", 
+				"-o","/Users/roberts/git/compiler/test/datagen/q20_output" };
+		BoaEvaluator.main(args);
+		String expected = "TransientMax[] = 140492550, 3.0\n"
+				+ "TransientMean[] = 0.0410958904109589\n"
+				+ "TransientMin[] = 140492550, 0.0\n"
+				+ "TransientTotal[] = 3\n"
+				+ "VolatileMax[] = 140492550, 1.0\n"
+				+ "VolatileMean[] = 0.0136986301369863\n"
+				+ "VolatileMin[] = 140492550, 0.0\n"
+				+ "VolatileTotal[] = 1\n";
+		File outputDir = new File("/Users/roberts/git/compiler/test/datagen/q20_output");
+		String actual = getResults(outputDir);// evaluator.getResults();
+		try {
+			FileUtils.deleteDirectory(outputDir);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(expected, actual);
 	}
-	assertEquals(expected, actual);
-	}
-	
+
 	public String getResults(File outputDir) {
-		for (final File f :  outputDir.listFiles()) {
+		for (final File f : outputDir.listFiles()) {
 			if (f.getName().startsWith("part")) {
 				return FileIO.readFileContents(f);
 			}
