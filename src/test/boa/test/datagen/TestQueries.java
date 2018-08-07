e8251f10d67e729c6c9aea1251040599e222cf80
robert2 <robert2@iastate.edu>
2018-08-07 10:25:57 -0500

    added testBinary_lit

--- src/test/boa/test/datagen/TestQueries.java
+++ src/test/boa/test/datagen/TestQueries.java
@@ -309,6 +309,79 @@ public class TestQueries {
 		assertEquals(expected, actual);
 	}
 	
+	@Test
+	public void testBinary_lit() {
+		try {
+			Process p = Runtime.getRuntime().exec("./boa.sh -e "
+					+ "-i test/known-good/binary-lit.boa "
+					+ "-d test/datagen/test_datagen "
+					+ "-o test/datagen/binary_lit_output");
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String s = null;
+			 while ((s = stdInput.readLine()) != null) {
+	                System.out.println(s);
+	            }
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		String expected = "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531520725000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n"
+				+ "USES[https://github.com/boalang/test-datagen][src/JLS4/BinLitDemo/BinLitDemo.java][1531880750000000] = 1\n";
+		File outputDir = new File("test/datagen/binary_lit_output");
+		String actual = getResults(outputDir);// evaluator.getResults();
+		try {
+			FileUtils.deleteDirectory(outputDir);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(expected, actual);
+	}
+	
 	public String getResults(File outputDir) {
 		for (final File f : outputDir.listFiles()) {
 			if (f.getName().startsWith("part")) {
