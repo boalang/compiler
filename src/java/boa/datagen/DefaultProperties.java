6094c99585acc32705ff38438085dd49cfffcb5e
robert2 <robert2@iastate.edu>
2018-08-17 08:06:22 -0500

    Lower default Max_size and Max_Commits.

--- src/java/boa/datagen/DefaultProperties.java
+++ src/java/boa/datagen/DefaultProperties.java
@@ -25,8 +25,8 @@ import java.io.File;
 public class DefaultProperties {
 	public static String NUM_THREADS = "1";
 	public static String MAX_PROJECTS = "1000";
-	public static String MAX_COMMITS = "50000";
-	public static String MAX_SIZE_FOR_PROJECT_WITH_COMMITS = String.valueOf(1 << 28); // Integer.MAX_VALUE / 3
+	public static String MAX_COMMITS = "25000";
+	public static String MAX_SIZE_FOR_PROJECT_WITH_COMMITS = String.valueOf(1 << 26); // Integer.MAX_VALUE / 3
 	
 	public static boolean DEBUG = false, DEBUGPARSE = false, CACHE = false;
 
