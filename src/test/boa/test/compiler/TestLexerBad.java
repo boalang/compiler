95bd4b4fe3662fc40ee19ef61641e37c47f5dcc4
Robert Dyer <rdyer@bgsu.edu>
2018-12-31 06:50:12 -0500

    add test cases for bad lexing of views

--- src/test/boa/test/compiler/TestLexerBad.java
+++ src/test/boa/test/compiler/TestLexerBad.java
@@ -40,4 +40,41 @@ public class TestLexerBad extends BaseTest {
 			new String[] { "bad", "comment", "<EOF>" },
 			new String[] { "1,0: token recognition error at: '@ '" });
 	}
+
+	@Test
+	public void badView1() throws IOException {
+		lex(load(badDir + "bad-view1.boa"),
+			new int[] { BoaLexer.Identifier, BoaLexer.EOF },
+			new String[] { "J0", "<EOF>" });
+	}
+
+	@Test
+	public void badView2() throws IOException {
+		lex(load(badDir + "bad-view2.boa"),
+			new int[] { BoaLexer.Identifier, BoaLexer.DIV, BoaLexer.EOF },
+			new String[] { "J12345", "/", "<EOF>" });
+	}
+
+	@Test
+	public void badView3() throws IOException {
+		lex(load(badDir + "bad-view3.boa"),
+			new int[] { BoaLexer.EOF },
+			new String[] { "<EOF>" },
+            new String[] { "1,0: token recognition error at: '@rdyer\\n'" });
+	}
+
+	@Test
+	public void badView4() throws IOException {
+		lex(load(badDir + "bad-view4.boa"),
+			new int[] { BoaLexer.EOF },
+			new String[] { "<EOF>" },
+            new String[] { "1,0: token recognition error at: '@rdyer/\\n'" });
+	}
+
+	@Test
+	public void badView5() throws IOException {
+		lex(load(badDir + "bad-view5.boa"),
+			new int[] { BoaLexer.Identifier, BoaLexer.DIV, BoaLexer.EOF },
+			new String[] { "v", "/", "<EOF>" });
+	}
 }
