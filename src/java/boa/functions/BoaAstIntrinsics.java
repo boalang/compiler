0be00b94dd61a7542e82cbc5fa1ee1ab49fd21c5
Robert Dyer <rdyer@bgsu.edu>
2019-03-01 01:53:50 -0500

    minor fixes to prettyprint (spacing issues)

--- src/java/boa/functions/BoaAstIntrinsics.java
+++ src/java/boa/functions/BoaAstIntrinsics.java
@@ -836,8 +836,7 @@ public class BoaAstIntrinsics {
 		String s = "";
 
 		if (n.getName().length() > 0) {
-			if (n.getModifiersCount() > 0)
-				s += prettyprint(n.getModifiersList()) + " ";
+			s += prettyprint(n.getModifiersList());
 			s += indent() + "package " + n.getName() + ";\n";
 		}
 
@@ -854,7 +853,7 @@ public class BoaAstIntrinsics {
 	public static String prettyprint(final Declaration d) {
 		if (d == null) return "";
 
-		String s = indent() + prettyprint(d.getModifiersList()) + " ";
+		String s = indent() + prettyprint(d.getModifiersList());
 
 		switch (d.getKind()) {
 			case INTERFACE:
@@ -958,10 +957,7 @@ public class BoaAstIntrinsics {
 	@FunctionSpec(name = "prettyprint", returnType = "string", formalParameters = { "Method" })
 	public static String prettyprint(final Method m) {
 		if (m == null) return "";
-		String s = indent();
-
-		for (int i = 0; i < m.getModifiersCount(); i++)
-			s += prettyprint(m.getModifiers(i)) + " ";
+		String s = indent() + prettyprint(m.getModifiersList());
 
 		if (m.getGenericParametersCount() > 0) {
 			s += "<";
@@ -979,12 +975,12 @@ public class BoaAstIntrinsics {
 				s += ", ";
 			s += prettyprint(m.getArguments(i));
 		}
-		s += ") ";
+		s += ")";
 
 		if (m.getExceptionTypesCount() > 0) {
-			s += "throws ";
+			s += " throws";
 			for (int i = 0; i < m.getExceptionTypesCount(); i++)
-				s += prettyprint(m.getExceptionTypes(i)) + " ";
+				s += " " + prettyprint(m.getExceptionTypes(i));
 		}
 
 		s += "\n";
@@ -1333,8 +1329,7 @@ public class BoaAstIntrinsics {
 				return prettyprint(e.getAnnotation());
 
 			case VARDECL:
-				for (int i = 0; i < e.getVariableDecls(0).getModifiersCount(); i++)
-					s += prettyprint(e.getVariableDecls(0).getModifiers(i)) + " ";
+				s += prettyprint(e.getVariableDecls(0).getModifiersList());
 				s += prettyprint(e.getVariableDecls(0).getVariableType()) + " ";
 				for (int i = 0; i < e.getVariableDeclsCount(); i++) {
 					if (i > 0)
