60afc7670fac4c1899bdf8e6ade0e6438eb77d85
yijiahuang <hyj@iastate.edu>
2019-03-08 12:43:10 -0600

    add new expression test case for prettyprint

--- src/test/boa/test/functions/TestPrettyprint.java
+++ src/test/boa/test/functions/TestPrettyprint.java
@@ -35,45 +35,45 @@ import org.junit.runners.Parameterized.Parameters;
  */
 @RunWith(Parameterized.class)
 public class TestPrettyprint {
-    private final static String CLASS_START = "class c {\n";
-    private final static String CLASS_END = "}\n";
-    private final static String METHOD_START = "\tvoid m()\n\t{";
-    private final static String METHOD_END = "\n\t}\n";
+	private final static String CLASS_START = "class c {\n";
+	private final static String CLASS_END = "}\n";
+	private final static String STATEMENT_START = CLASS_START + "\tvoid m()\n\t{";
+	private final static String STATEMENT_END = "\n\t}\n" + CLASS_END;
 
-    @Parameters
-    public static Collection<String[]> code() {
-        return Arrays.asList(new String[][]{
-            /* classes */
-            { "class c {\n}\n" },
-            { "public class c {\n}\n" },
-            { "class c extends d {\n}\n" },
-            { "class c implements i1 {\n}\n" },
-            { "class c implements i1, i2, i3 {\n}\n" },
-            { "abstract static final private class c extends d implements i1, i2, i3 {\n}\n" },
+	@Parameters
+	public static Collection<String[]> code() {
+		return Arrays.asList(new String[][] {
+				/* classes */
+				{ "class c {\n}\n" }, { "public class c {\n}\n" }, { "class c extends d {\n}\n" },
+				{ "class c implements i1 {\n}\n" }, { "class c implements i1, i2, i3 {\n}\n" },
+				{ "abstract static final private class c extends d implements i1, i2, i3 {\n}\n" },
 
-            /* methods */
-            { CLASS_START + "\tvoid m()\n\t{\n\t}\n" + CLASS_END },
-            { CLASS_START + "\tint m()\n\t{\n\t\treturn 1;\n\t}\n" + CLASS_END },
-            
-            /* statements */
-            { CLASS_START + METHOD_START + "\n\t\tswitch (f1) {"
-            		+ "\n\t\t\tcase 1:"
-            		+ "\n\t\t\tf1 = 2;"
-            		+ "\n\t\t\tdefault:"
-            		+ "\n\t\t\tbreak;\n\t\t}" 
-            		+ METHOD_END + CLASS_END } // switch
-        });
-    }
+				/* methods */
+				{ CLASS_START + "\tvoid m()\n\t{\n\t}\n" + CLASS_END },
+				{ CLASS_START + "\tint m()\n\t{\n\t\treturn 1;\n\t}\n" + CLASS_END },
 
-    private String code;
+				/* statements */
+				{ STATEMENT_START + "\n\t\tswitch (f1) {" 
+						+ "\n\t\t\tcase 1:" 
+						+ "\n\t\t\tf1 = 2;" 
+						+ "\n\t\t\tdefault:"
+						+ "\n\t\t\tbreak;\n\t\t}" + STATEMENT_END }, // SWITCH
+				
+				/* expressions */
+				{ STATEMENT_START + "\n\t\tList<String> list = new ArrayList<String>();" + STATEMENT_END} // NEW
+		});
+	}
 
-    public TestPrettyprint(final String code) {
-        this.code = code;
-    }
+	private String code;
 
-    @Test()
-    public void testPrettyprint() throws Exception {
-    	System.out.println(prettyprint(parse(code)));
-        assertEquals(code, prettyprint(parse(code)));
-    }
+	public TestPrettyprint(final String code) {
+		this.code = code;
+	}
+
+	@Test()
+	public void testPrettyprint() throws Exception {
+		String expected = prettyprint(parse(code));
+		System.out.println(expected);
+		assertEquals(code, expected);
+	}
 }
