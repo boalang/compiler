5c9f3cfbb499207d005d3949fea1d4923a267173
yijiahuang <hyj@iastate.edu>
2020-04-17 20:04:17 -0500

    minor change in evaluator

--- src/java/boa/evaluator/BoaEvaluator.java
+++ src/java/boa/evaluator/BoaEvaluator.java
@@ -125,7 +125,7 @@ public class BoaEvaluator extends BoaMain {
 
 			method.invoke(null, (Object)actualArgs);
 		} catch (final Throwable e) {
-			System.err.print(e);
+			System.err.print(e.getCause());
 		} finally {
 			if (cl != null)
 				try {
