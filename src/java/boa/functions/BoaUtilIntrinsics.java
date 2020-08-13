1a26a37181067cb4a0c81b645cd0e9b545cf7b9d
yijiahuang <hyj@iastate.edu>
2020-08-12 19:15:36 -0500

    add association rule examples

--- src/java/boa/functions/BoaUtilIntrinsics.java
+++ src/java/boa/functions/BoaUtilIntrinsics.java
@@ -7,8 +7,12 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
-import org.apache.commons.lang3.StringUtils;
 
+import org.apache.commons.compress.utils.Lists;
+import org.apache.commons.lang3.StringUtils;
+import org.nd4j.shade.guava.collect.Sets;
+
+import boa.runtime.BoaAbstractVisitor;
 import boa.types.Ast.*;
 import boa.types.Code.CodeRepository;
 import boa.types.Code.Revision;
@@ -67,7 +71,7 @@ public class BoaUtilIntrinsics {
 
 		// add method name
 		sb.append(m.getName() + "(");
-		
+
 		// add method arguments
 		for (int i = 0; i < m.getArgumentsCount(); i++) {
 			if (i > 0)
@@ -136,7 +140,7 @@ public class BoaUtilIntrinsics {
 
 	private static void update(Entry<String, LinkedList<Method>> e1, Entry<String, LinkedList<Method>> e2,
 			List<String[]> results) {
-		
+
 		HashMap<String, Method> deleted = new HashMap<String, Method>();
 		for (Method m : e1.getValue())
 			deleted.put(signature(m), m);
@@ -144,7 +148,7 @@ public class BoaUtilIntrinsics {
 		HashMap<String, Method> added = new HashMap<String, Method>();
 		for (Method m : e2.getValue())
 			added.put(signature(m), m);
-		
+
 		for (Iterator<Entry<String, Method>> itr = deleted.entrySet().iterator(); itr.hasNext();) {
 			String sig = itr.next().getKey();
 			if (added.containsKey(sig)) {
@@ -152,12 +156,12 @@ public class BoaUtilIntrinsics {
 				added.remove(sig);
 			}
 		}
-		
+
 		if (deleted.size() == 0 || added.size() == 0)
 			return;
 
 		String fqn = e1.getKey();
-		
+
 		for (Iterator<Entry<String, Method>> itr1 = deleted.entrySet().iterator(); itr1.hasNext();) {
 			Entry<String, Method> entry1 = itr1.next();
 			for (Iterator<Entry<String, Method>> itr2 = added.entrySet().iterator(); itr2.hasNext();) {
@@ -174,7 +178,7 @@ public class BoaUtilIntrinsics {
 						break;
 					}
 				}
-				
+
 			}
 		}
 	}
@@ -193,23 +197,173 @@ public class BoaUtilIntrinsics {
 				return false;
 		return true;
 	}
-	
+
+	@FunctionSpec(name = "evolution_apis", returnType = "array of array of string", formalParameters = {
+			"map[string] of queue of Method", "map[string] of queue of Method" })
+	public static String[][] evolutionAPIs(HashMap<String, LinkedList<Method>> prev,
+			HashMap<String, LinkedList<Method>> cur) throws Exception {
+		List<String[]> results = new ArrayList<>();
+		for (Entry<String, LinkedList<Method>> e1 : prev.entrySet())
+			for (Entry<String, LinkedList<Method>> e2 : cur.entrySet())
+				if (e1.getKey().equals(e2.getKey())) {
+					matchMethods(e1, e2, results);
+					break;
+				}
+		String[][] res = new String[results.size()][2];
+		for (int i = 0; i < res.length; i++)
+			res[i] = results.get(i);
+		return res;
+	}
+
+	private static void matchMethods(Entry<String, LinkedList<Method>> e1, Entry<String, LinkedList<Method>> e2,
+			List<String[]> results) throws Exception {
+		HashMap<String, Method> deleted = new HashMap<String, Method>();
+		for (Method m : e1.getValue())
+			deleted.put(signature(m), m);
+
+		HashMap<String, Method> added = new HashMap<String, Method>();
+		for (Method m : e2.getValue())
+			added.put(signature(m), m);
+
+		for (Iterator<Entry<String, Method>> itr = deleted.entrySet().iterator(); itr.hasNext();) {
+			Entry<String, Method> before = itr.next();
+			String sig = before.getKey();
+			if (added.containsKey(sig)) {
+				Method beforeMethod = before.getValue();
+				Method afterMethod = added.get(sig);
+				if (!matchMethodBody2(beforeMethod, afterMethod))
+					findAPIEvolutionPairs(beforeMethod, afterMethod, results);
+				itr.remove();
+				added.remove(sig);
+			}
+		}
+	}
+
+	private static boolean matchMethodBody2(Method m1, Method m2) {
+		if (m1.getStatementsCount() == 0 || m2.getStatementsCount() == 0)
+			return true;
+		List<Statement> s1 = m1.getStatements(0).getStatementsList();
+		List<Statement> s2 = m2.getStatements(0).getStatementsList();
+		if (s1.size() != s2.size())
+			return false;
+		if (s1.size() == 0 || s2.size() == 0)
+			return true;
+		for (int i = 0; i < s1.size(); i++)
+			if (!prettyprint(s1.get(i)).equals(prettyprint(s2.get(i))))
+				return false;
+		return true;
+	}
+
+	private static void findAPIEvolutionPairs(Method m1, Method m2, List<String[]> results) throws Exception {
+		HashSet<String> deletedTypes = collectTypes(m1);
+		HashSet<String> addedTypes = collectTypes(m2);
+
+		HashSet<String> commons = Sets.newHashSet(deletedTypes);
+		commons.retainAll(addedTypes);
+
+		deletedTypes.removeAll(commons);
+		addedTypes.removeAll(commons);
+
+		if (deletedTypes.size() == 0 || addedTypes.size() == 0)
+			return;
+
+		for (String deletedType : deletedTypes)
+			for (String addedType : addedTypes)
+				results.add(new String[] { deletedType, addedType });
+	}
+
+	private static HashSet<String> collectTypes(Method m) {
+		HashSet<String> types = Sets.newHashSet();
+		BoaAbstractVisitor v = new BoaAbstractVisitor() {
+			@Override
+			protected boolean preVisit(final Type type) throws Exception {
+				types.add(type.getName());
+				return false;
+			}
+		};
+		try {
+			v.visit(m);
+		} catch (Exception e) {
+			return types;
+		}
+		return types;
+	}
+
+	@FunctionSpec(name = "cousage_apis", returnType = "array of array of string", formalParameters = {
+			"map[string] of queue of Method", "map[string] of queue of Method" })
+	public static String[][] cousageAPIs(HashMap<String, LinkedList<Method>> prev,
+			HashMap<String, LinkedList<Method>> cur) throws Exception {
+		List<String[]> results = new ArrayList<>();
+		for (Entry<String, LinkedList<Method>> e1 : prev.entrySet())
+			for (Entry<String, LinkedList<Method>> e2 : cur.entrySet())
+				if (e1.getKey().equals(e2.getKey())) {
+					matchMethods2(e1, e2, results);
+					break;
+				}
+		String[][] res = new String[results.size()][2];
+		for (int i = 0; i < res.length; i++)
+			res[i] = results.get(i);
+		return res;
+	}
+
+	private static void matchMethods2(Entry<String, LinkedList<Method>> e1, Entry<String, LinkedList<Method>> e2,
+			List<String[]> results) throws Exception {
+		HashMap<String, Method> deleted = new HashMap<String, Method>();
+		for (Method m : e1.getValue())
+			deleted.put(signature(m), m);
+
+		HashMap<String, Method> added = new HashMap<String, Method>();
+		for (Method m : e2.getValue())
+			added.put(signature(m), m);
+
+		for (Iterator<Entry<String, Method>> itr = deleted.entrySet().iterator(); itr.hasNext();) {
+			Entry<String, Method> before = itr.next();
+			String sig = before.getKey();
+			if (added.containsKey(sig)) {
+				Method beforeMethod = before.getValue();
+				Method afterMethod = added.get(sig);
+				if (!matchMethodBody2(beforeMethod, afterMethod))
+					findAPICousagePairs(beforeMethod, afterMethod, results);
+				itr.remove();
+				added.remove(sig);
+			}
+		}
+	}
+
+	private static void findAPICousagePairs(Method m1, Method m2, List<String[]> results) {
+		HashSet<String> deletedTypes = collectTypes(m1);
+		HashSet<String> addedTypes = collectTypes(m2);
+
+		HashSet<String> commons = Sets.newHashSet(deletedTypes);
+		commons.retainAll(addedTypes);
+
+		deletedTypes.removeAll(commons);
+		addedTypes.removeAll(commons);
+
+		List<String> added = new ArrayList<>(addedTypes);
+		for (int i = 0; i < added.size(); i++) {
+			for (int j = i + 1; j < added.size(); j++) {
+				results.add(new String[] { added.get(i), added.get(j) });
+			}
+		}
+	}
+
 //	public static void main(String[] args) {
 //		String s = "camelCased<List<Integer3[seq2vec]>>_____and_UNDERSCORED<HE>[SHE]_.....__camelCasedDDDDD";
 //		System.out.println(Arrays.toString(split(s)));
 //		System.out.println(alphabetFilter(s, "_."));
-		
+
 //		System.out.println(bytesOfString(500));
 //		System.out.println(calculateRecordPercent(100, bytesOfString(500)));
 //	}
-	
+
 	/*
 	 * web: https://www.javamex.com/tutorials/memory/string_memory_usage.shtml
 	 */
 	public static int bytesOfString(int numOfChars) {
 		return 8 * (int) (((numOfChars * 2) + 45) / 8);
 	}
-	
+
 	public static float calculateRecordPercent(int combinerMem, int recordMem) {
 		float maxRecords = (float) combinerMem * (1 << 20) / recordMem;
 		MAX_RECORDS_FOR_SPILL = (int) maxRecords + 10;
