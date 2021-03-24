e8870bbdc6ec18409ef6d99e0d93f3317491d58d
Yijia Huang <hyjorc1@gmail.com>
2021-03-23 19:34:34 -0500

    add ensemble mode to naive bayes aggregator

--- src/java/boa/aggregators/ml/weka/NaiveBayesAggregator.java
+++ src/java/boa/aggregators/ml/weka/NaiveBayesAggregator.java
@@ -17,16 +17,21 @@
 package boa.aggregators.ml.weka;
 
 import boa.aggregators.AggregatorSpec;
+import boa.io.EmitKey;
+import boa.io.EmitValue;
 import boa.runtime.Tuple;
 import weka.classifiers.bayes.NaiveBayes;
 import java.io.IOException;
+
+import org.apache.hadoop.mapreduce.Reducer;
 
 /**
  * A Boa aggregator for training the model using LinearRegression.
  *
  * @author ankuraga
+ * @author hyj
  */
-@AggregatorSpec(name = "naivebayes", formalParameters = { "string" })
+@AggregatorSpec(name = "naivebayes", formalParameters = { "string" }, canCombine = true)
 public class NaiveBayesAggregator extends MLAggregator {
 	private NaiveBayes model;
 
@@ -60,12 +65,22 @@ public class NaiveBayesAggregator extends MLAggregator {
 			this.model = new NaiveBayes();
 			this.model.setOptions(options);
 			this.model.buildClassifier(this.instances);
+			this.saveModel(this.model);
+			System.out.println("trained NaiveBayes model");
+			
+			if (trainWithCombiner) {
+				@SuppressWarnings("unchecked")
+				Reducer<EmitKey, EmitValue, EmitKey, EmitValue>.Context context = getContext();
+				EmitKey key = getKey();
+				// pass the path of trained model
+				context.write(key, new EmitValue(modelPath.toString(), "model_path"));
+			} else {
+				String info = "\n=== Model Info ===\n" + this.model.toString();
+				this.collect(info);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		this.saveModel(this.model);
-		String info = "\n=== Model Info ===\n" + this.model.toString();
-		this.collect(info);
 	}
 
 }
