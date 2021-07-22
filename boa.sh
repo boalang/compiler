c41b674fce6bb8419a56567147e4cbce1dad1257
Robert Dyer <rdyer@unl.edu>
2021-07-22 13:13:48 -0500

    add kotlin jars to scripts

--- boa.sh
+++ boa.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(dirname "$0")
 
-java -cp ".:$BASEDIR/dist/boa-compiler.jar:$BASEDIR/lib/*:$BASEDIR/lib/evaluator/*:$BASEDIR/lib/datagen/*:$BASEDIR/compile" boa.BoaMain $*
+java -cp ".:$BASEDIR/dist/boa-compiler.jar:$BASEDIR/lib/*:$BASEDIR/lib/evaluator/*:$BASEDIR/lib/datagen/*:$BASEDIR/lib/datagen-kotlin/*:$BASEDIR/compile" boa.BoaMain $*
