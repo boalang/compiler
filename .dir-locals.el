c5e4b4dfd97f1644c419efee94c24cb07a6818cf
Samuel W. Flint <swflint@flintfam.org>
2021-07-26 14:46:44 -0500

    Add indentation configuration for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((java-mode . ((tab-width . 8)
+               (c-basic-offset . 8)
+               (indent-tabs-mode . t)
+               (ws-trim-mode . t)
+               (eval . (progn
+                         (c-set-offset 'arglist-cont-nonempty '++)
+                         (c-set-offset 'case-label '+))))))
