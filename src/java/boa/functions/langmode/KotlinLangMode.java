82cb5d3b19f180dd95e767c0893bef7966bbce56
Samuel W. Flint <swflint@flintfam.org>
2021-07-26 14:43:22 -0500

    Add a basic array comprehension case

--- src/java/boa/functions/langmode/KotlinLangMode.java
+++ src/java/boa/functions/langmode/KotlinLangMode.java
@@ -597,6 +597,7 @@ public class KotlinLangMode implements LangMode {
 			case ASSIGN_LSHIFT:         return ppInfix("<<=",  e.getExpressionsList());
 			case ASSIGN_RSHIFT:         return ppInfix(">>=",  e.getExpressionsList());
 			case ASSIGN_UNSIGNEDRSHIFT: return ppInfix(">>>=", e.getExpressionsList());
+			case ARRAY_COMPREHENSION:   return ppInfix("..", e.getExpressionsList());
 
 			case LOGICAL_NOT: return ppPrefix("!", e);
 			case BIT_NOT:     return ppPrefix("~", e);
