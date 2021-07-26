64d015a754cd56d8d1270e6448d92564970a06d6
Samuel W. Flint <swflint@flintfam.org>
2021-07-26 14:58:08 -0500

    Add protobuf indentation rules for Emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -7,4 +7,8 @@
                (ws-trim-mode . t)
                (eval . (progn
                          (c-set-offset 'arglist-cont-nonempty '++)
-                         (c-set-offset 'case-label '+))))))
+                         (c-set-offset 'case-label '+)))))
+ (protobuf-mode . ((tab-width . 8)
+                   (c-basic-offset . 8)
+                   (indent-tabs-mode . t)
+                   (ws-trim-mode . t))))
