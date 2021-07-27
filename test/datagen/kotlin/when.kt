3b8ab5e4d7ba3329eedfe8c3ff163bd6b5fb7128
Robert Dyer <rdyer@unl.edu>
2021-07-27 12:46:08 -0500

    update when test

--- test/datagen/kotlin/when.kt
+++ test/datagen/kotlin/when.kt
@@ -1,17 +1,27 @@
 fun f() : Unit {
     when (x) {
         is Int -> print(x)
-        in 1..5 -> print(x)
+        in 1..5 -> {
+            print(x)
+            print(x)
+        }
         "foo" -> print(x)
-        else -> print(x)
+        else -> {
+            print(x)
+        }
     }
 }
 
 var x = when(x) {
     when (x) {
-        is Int -> print(x)
-        in 1..5 -> print(x)
+        !is Int -> print(x)
+        !in 1..5 -> {
+            print(x)
+            print(x)
+        }
         "foo" -> print(x)
-        else -> print(x)
+        else -> {
+            print(x)
+        }
     }
 }
