2135f37e69ca61a5bdc4888a6e22464b8274f8f4
Robert Dyer <rdyer@unl.edu>
2021-07-28 16:00:23 -0500

    update ranged test

--- test/datagen/kotlin/ranged-for.kt
+++ test/datagen/kotlin/ranged-for.kt
@@ -2,6 +2,12 @@ fun f() {
     for (i in 1..5) {
         println(i)
     }
+    for (i in 1..5 step 5) {
+        println(i)
+    }
+    for (j in 60 downto 3) {
+        println(j)
+    }
     for (j in 60 downto 3 step 3) {
         println(j)
     }
