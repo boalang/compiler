fc5241cbaa4a021b619968d9903f917cd83cab07
Robert Dyer <rdyer@unl.edu>
2021-07-28 13:01:18 -0500

    add test

---
+++ test/datagen/kotlin/do-while.kt
@@ -0,0 +1,5 @@
+fun dowhile() {
+    do {                                                                                                           
+        print("hi")                                                                                                     
+    } while (x)                                                                                                         
+}
