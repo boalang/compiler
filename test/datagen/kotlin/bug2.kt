906356a24c45dc07a0a1be6237c53c6e91c47ee2
Robert Dyer <rdyer@unl.edu>
2021-07-29 09:55:33 -0500

    fix bug2 test

--- test/datagen/kotlin/bug2.kt
+++ test/datagen/kotlin/bug2.kt
@@ -1,4 +1,3 @@
 fun f() {
-    (1..Math.sqrt(number.toDouble()).toInt() step if (number % 2 == 0) 1 else 2)
-        .forEach { if (number % it == 0) divisors.add(it) }
+    (1..5 step if (number % 2 == 0) 1 else 2)
 }
