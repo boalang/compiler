b66ae9eb69386f69b4e8f4571d25e999de4de3d0
Samuel W. Flint <swflint@flintfam.org>
2021-08-07 13:37:03 -0500

    Create a new smaller test for constraints on a field in a class

---
+++ test/datagen/kotlin/type-constraints-val-field-in-class.kt
@@ -0,0 +1,13 @@
+class foo<T : S, P>
+    where T : R,
+          P : M,
+          T : Q {
+    val b = fun <T: A> ()
+        where T: B,
+              T : T<C>,
+              T : D
+    val <T> c
+        where T: B,
+              T : T<C>,
+              T : D
+}
