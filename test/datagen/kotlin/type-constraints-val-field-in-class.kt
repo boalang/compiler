e0d60404c658c36f910479263c3cd98d08ea697d
Robert Dyer <rdyer@unl.edu>
2021-08-08 10:59:23 -0500

    simplify testcase

--- test/datagen/kotlin/type-constraints-val-field-in-class.kt
+++ test/datagen/kotlin/type-constraints-val-field-in-class.kt
@@ -1,11 +1,4 @@
-class foo<T : S, P>
-    where T : R,
-          P : M,
-          T : Q {
-    val b = fun <T: A> ()
-        where T: B,
-              T : T<C>,
-              T : D
+class foo {
     val <T> c
         where T: B,
               T : T<C>,
