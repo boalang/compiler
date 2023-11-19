74d48e59056365aa4fa91e109d81a9e0a53a6034
Robert Dyer <rdyer@unl.edu>
2023-11-19 01:56:16 -0600

    add isfork() function

--- src/java/boa/functions/BoaIntrinsics.java
+++ src/java/boa/functions/BoaIntrinsics.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2021, Hridesh Rajan, Robert Dyer,
+ * Copyright 2014-2023, Hridesh Rajan, Robert Dyer,
  *                 Iowa State University of Science and Technology
  *                 and University of Nebraska Board of Regents
  *
@@ -17,6 +17,8 @@
  */
 package boa.functions;
 
+import java.io.BufferedInputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -29,6 +31,12 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import boa.datagen.DefaultProperties;
 import boa.types.Code.CodeRepository;
 import boa.types.Code.Revision;
 import boa.types.Diff.ChangedFile;
@@ -41,6 +49,43 @@ import boa.types.Toplevel.Project;
  * @author rdyer
  */
 public class BoaIntrinsics {
+	private static Set<Integer> forksData;
+
+	private static void loadForksData() {
+		try {
+			final Configuration conf = BoaAstIntrinsics.context.getConfiguration();
+			final FileSystem fs;
+			final Path p;
+			if (DefaultProperties.localDataPath != null) {
+				p = new Path(DefaultProperties.localDataPath, "forks.bin");
+				fs = FileSystem.getLocal(conf);
+			} else {
+				p = new Path(
+					BoaAstIntrinsics.context.getConfiguration().get("fs.default.name", "hdfs://boa-njt/"),
+					new Path(conf.get("boa.forks.file", conf.get("boa.ast.dir", conf.get("boa.input.dir", "")) + "/forks.bin"))
+				);
+				fs = FileSystem.get(conf);
+			}
+
+			try (final FSDataInputStream data = fs.open(p);
+				final BufferedInputStream bis = new BufferedInputStream(data);
+				final ObjectInputStream ois = new ObjectInputStream(bis)) {
+				forksData = (Set<Integer>)ois.readObject();
+			}
+		} catch (final Exception e) {
+			System.err.println("Error reading forks.bin: " + e.getMessage());
+			e.printStackTrace();
+			forksData = new HashSet<Integer>();
+		}
+	}
+
+	@FunctionSpec(name = "isfork", returnType = "bool", formalParameters = { "Project" })
+	public static boolean isfork(final Project p) {
+		if (forksData == null)
+			loadForksData();
+		return p.getForked() || forksData.contains(Integer.parseInt(p.getId()));
+	}
+
 	private final static String[] fixingRegex = {
 		"\\bfix(s|es|ing|ed)?\\b",
 		"\\b(error|bug|issue)(s)?\\b",
